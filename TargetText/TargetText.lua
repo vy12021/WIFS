4422e090ae4aec4c3595d0c0edb1fcb6fd8e2c91
Tesla <vy12021@gmail.com>
2014-08-05 13:47:01 +0800

    目标信息显示插件添加功能

--- TargetText/TargetText.lua
+++ TargetText/TargetText.lua
@@ -49,6 +49,11 @@ local events = {
     "UNIT_MAXFOCUS",
 }
 
+local colors = {
+    white = {r = 1, g = 1, b = 1},
+    blue = {r = 0, g = 0, b = 1},
+}
+
 function TargetText:RegisterUpdates(event)
     for _, event in ipairs(events) do
         TargetTextFrame:RegisterEvent(event)
@@ -77,3 +82,116 @@ function TargetText:UpdateAll()
         TargetTextFrame:Hide()
     end
 end
+
+function TargetText:UpdateName()
+    local name = UnitName("target") or ""
+    TargetTextFrameName:SetText(name)
+end
+
+function TargetText:UpdatePvP()
+    if UnitIsPVP("target") then
+        TargetTextFramePvP:Show()
+    else
+        TargetTextFramePvP:Hide()
+    end
+end
+
+function TargetText:UpdatePowerType()
+    local info = ManaBarColor[UnitPowerType("target")]
+    TargetTextFrameManaLabel:SetText(info.prefix)
+    TargetTextFrameMana:SetTextColor(info.r, info.g, info.b)
+end
+
+function TargetText:UpdateClass()
+    local class, key = UnitClass("target")
+    local color = RAID_CLASS_COLORS[key]
+    TargetTextFrameClass:SetText(class)
+    TargetTextFrameClass:SetTextColor(color.r, color.g, color.b)
+end
+
+function TargetText:UpdateMana()
+    local mana, maxMana = UnitMana("target"), UnitManaMax("target")
+    local precent = mana / maxMana
+    local manaText
+    if maxMana == 0 then
+        manaText = "N/A"
+    else
+    manaText = string.format("%d/%d (%.0f%%)", mana, maxMana, precent * 100)
+    end
+
+    TargetTextFrameMana:SetText(manaText)
+end
+
+function TargetText:UpdateHealth()
+    local health, maxHealth = UnitHealth("target"), UnitHealthMax("target")
+    local percent = health / maxHealth
+    local healthText
+    if maxHealth == 0 then
+        healthText = "N/A"
+    elseif maxHealth == 100 then
+        healthText = string.format("%.0f%%", percent * 100)
+    else
+        healthText = string.format("%d%d (%.0f%%)", health, maxHealth, percent * 100)
+    end
+
+    TargetTextFrameHealth:SetText(healthText)
+    TargetTextFrameHealth:SetTextColor(self:GetHealthColor(percent))
+end
+
+function TargetText:GetHealthColor(percent)
+    local red, green
+
+    if percent >= 0.5 then
+        red = (1 - percent) * 2
+        green = 1
+    else
+        red = 1
+        green = percent * 2
+    end
+
+    return red, green, 0
+end
+
+function TargetText:UpdateFaction()
+    local faction = select(2, UnitFactionGroup("target"))
+    local color
+
+    if not faction then
+        color = NORMAL_FONT_COLOR
+        faction = "No Faction"
+    elseif
+        faction == select(2, UnitFactionGroup("player")) then
+        color = GREEN_FONT_COLOR
+    else
+        color = RED_FONT_COLOR
+    end
+
+    TargetTextFrameFaction:SetText(faction)
+    TargetTextFrameFaction:SetTextColor(color.r, color.g, color.b)
+end
+
+function TargetText:UpdateLevel()
+    local level = UnitLevel("target")
+
+    local color
+    if UnitCanAttack("player", "target") then
+        color = GetDifficultyColor(level)
+    else
+        color = colors.white
+    end
+
+    if level == -1 then
+        level = "??"
+    end
+
+    local classfication = UnitClassification("target")
+    if classfication == "worldboss" or
+            classfication == "rareelite" or
+            classfication == "elite" then
+        level = level.."+"
+    end
+
+    TargetTextFrameLevel:SetText(level)
+    TargetTextFrameLevel:SetTextColor(color.r, color.g, color.b)
+end
+
